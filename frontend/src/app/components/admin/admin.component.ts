9bb939427ac99487d052e5b6e037cfad5d8de259
Wosbely11 <21wosbely11sac@gmail.com>
2026-04-24 13:40:25 -0600

    Varios

--- frontend/src/app/components/admin/admin.component.ts
+++ frontend/src/app/components/admin/admin.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
-import { AdminService } from '../../services/admin/admin.service'; // <-- Importamos el servicio
+import { AdminService } from '../../services/admin/admin.service'; 
 import { FormsModule } from '@angular/forms';
 
 @Component({
