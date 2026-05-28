c2e9befd9c26d4bc9ba7193cb2a3095fcb169dba
kazc93 <azurdia9693@gmail.com>
2026-05-27 23:15:11 -0600

    botón Mi Perfil

--- backend/post-service/jest.config.js
+++ backend/post-service/jest.config.js
@@ -3,6 +3,10 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', { diagnostics: false }],
+    '^.+\\.ts$': ['ts-jest', { diagnostics: false, isolatedModules: true }],
   },
+  collectCoverageFrom: [
+    'src/helpers/**/*.ts',
+    '!src/**/*.test.ts',
+  ],
 };
